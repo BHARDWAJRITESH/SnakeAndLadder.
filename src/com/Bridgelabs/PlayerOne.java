ff27efcf78922d9944f75967347bfa7f7cf276ff
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-22 10:52:03 +0530

    Add:-Player One at Initial position[0]

---
+++ src/com/Bridgelabs/PlayerOne.java
@@ -0,0 +1,13 @@
+package com.Bridgelabs;
+
+public class PlayerOne {
+	
+	//creating constructor
+	PlayerOne(){
+		int start = 0;
+		int playerPosition;
+		playerPosition = start;
+		
+		System.out.println("== Player Ritesh at Position:" + playerPosition +" ==");
+	}
+}
--- src/com/Bridgelabs/SnakeAndLadder.java
+++ src/com/Bridgelabs/SnakeAndLadder.java
@@ -1,10 +1,16 @@
 package com.Bridgelabs;
 
 public class SnakeAndLadder {
-
-	public static void main(String[] args) {
-		System.out.println("**Welcome to the snake and ladder programs**");
 		
+	public static void main(String[] args) {
+		
+		//welcome message
+		System.out.println("**Welcome to the snake and ladder programs**");
+		System.out.println("=======Game Start=======");
+		
+		//playerOne started the game
+		//calling constructor
+		PlayerOne ritesh = new PlayerOne();      								//calling constructor
 	}
 
 }
