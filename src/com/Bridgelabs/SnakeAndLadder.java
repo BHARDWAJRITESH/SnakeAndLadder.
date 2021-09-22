6d928519f5e1f7a583d5f08e1eae2d51b71e4d18
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-22 09:44:21 +0530

    Add:-Welcome message

---
+++ src/com/Bridgelabs/SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.Bridgelabs;
+
+public class SnakeAndLadder {
+
+	public static void main(String[] args) {
+		System.out.println("**Welcome to the snake and ladder programs**");
+		
+	}
+
+}
