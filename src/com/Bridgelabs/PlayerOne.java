eb998341c232926e6358eddb724e7dc48ef8bbd6
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-22 14:36:23 +0530

    Add:-PlayerOne[ritesh] counting dice roll to reach exact hundred mark

--- src/com/Bridgelabs/PlayerOne.java
+++ src/com/Bridgelabs/PlayerOne.java
@@ -2,58 +2,58 @@ package com.Bridgelabs;
 
 public class PlayerOne {
 	
-	//creating constructor
-	PlayerOne(){
-		int start = 0;
-		int playerPosition;
-		playerPosition = start;
-		
-		//setting position of player one
-		System.out.println("Player Ritesh at Position:" + playerPosition );
-		System.out.println();
-		
-		
-		
-		//using while loop for reaching winning position
-		while (playerPosition != 100) {
+	
+		//creating constructor
+		PlayerOne(){
+			int start = 0;
+			int playerPosition;
+			playerPosition = start;
+			int diceRollCount= 0;
 			
-			//rolling dice and recording output with help of random
-			int diceRoll = 1+(int)Math.floor(Math.random() *10)%6;
-			System.out.println("Player Ritesh Rolls the dice: " + diceRoll);
+			//setting position of player one
+			System.out.println("Player Ritesh at Position:" + playerPosition );
 			System.out.println();
 			
-			//check for option like snake,ladder and no play by help of random
-			int checkOption= (int)Math.floor(Math.random() *10)% 3;
-				if (checkOption == 0) {
-					System.out.println("======== No play ========");
-				}
-				else if(checkOption == 1) {
-					System.out.println("======== Ladder comes ========");
+			
+			
+			//using while loop for reaching winning position
+				while (playerPosition != 100) {
 				
-					// adding condition to reach winning position
-					playerPosition = playerPosition + diceRoll;
+				//rolling dice and recording output with help of random
+					int diceRoll = 1+(int)Math.floor(Math.random() *10)%6;
+					System.out.println("Player Ritesh Rolls the dice: " + diceRoll);
+					System.out.println();
+				
+				//check for option like snake,ladder and no play by help of random
+					int checkOption= (int)Math.floor(Math.random() *10)% 3;
+						if (checkOption == 0) {
+							System.out.println("======== No play ========");
+						}
+						else if(checkOption == 1) {
+							System.out.println("======== Ladder comes ========");
 					
-						if(playerPosition >100) {
+						// adding condition to reach winning position
+							playerPosition = playerPosition + diceRoll;
+							
+								if(playerPosition >100) {
+									playerPosition = playerPosition - diceRoll;
+								}
+								else if(playerPosition + diceRoll > 100) {
+								continue;
+								}
+						}
+						else {
+							System.out.println("======== Snake comes ========");
 							playerPosition = playerPosition - diceRoll;
+								if (playerPosition  < 0) 
+									playerPosition = 0;
+								} 
+							
+						diceRollCount ++;
 						}
-						else if(playerPosition + diceRoll > 100) {
-						
-							continue;
-						}
-				}
-				else {
-					System.out.println("======== Snake comes ========");
-					playerPosition = playerPosition - diceRoll;
-						if (playerPosition  < 0) 
-							playerPosition = 0;
-						} 
-						
-						
-		 }
+			
+				System.out.println("Current Position of player Ritesh is: "+playerPosition);
+				System.out.println("Number of times dice was rolled by player Ritesh: " +diceRollCount);
+		}	
 		
-		 System.out.println("Current Position of player Ritesh is: "+playerPosition);
-	   }	
 }
-
-	
-	
