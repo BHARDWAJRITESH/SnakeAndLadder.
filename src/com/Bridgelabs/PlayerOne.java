0c7121f9b29a5eba3c848fe9d422b71aeefe5203
BHARDWAJRITESH <ritesh844581@gmail.com>
2021-09-22 13:21:04 +0530

    Add:-PlayerOne[ritesh] playing the game untill he winns the game

--- src/com/Bridgelabs/PlayerOne.java
+++ src/com/Bridgelabs/PlayerOne.java
@@ -12,21 +12,40 @@ public class PlayerOne {
 		System.out.println("Player Ritesh at Position:" + playerPosition );
 		System.out.println();
 		
-		//rolling dice and recording output with help of random
-		int diceRoll = 1+(int)Math.floor(Math.random() *10)%6;
-		System.out.println("Player Ritesh Rolls the dice: " + diceRoll);
-		System.out.println();
 		
-		//check for option like snake,ladder and no play by help of random
-		int checkOption= (int)Math.floor(Math.random() *10)% 3;
-			if (checkOption == 0) {
-				System.out.println("======== No play ========");
-			}
-			else if(checkOption == 1) {
-				System.out.println("======== Ladder comes ========");
-			}
-			else {
-				System.out.println("======== Snake comes ========");
-		}
+		
+		//using while loop for reaching winning position
+		while (playerPosition != 100) {
+			
+			//rolling dice and recording output with help of random
+			int diceRoll = 1+(int)Math.floor(Math.random() *10)%6;
+			System.out.println("Player Ritesh Rolls the dice: " + diceRoll);
+			System.out.println();
+			
+			//check for option like snake,ladder and no play by help of random
+			int checkOption= (int)Math.floor(Math.random() *10)% 3;
+				if (checkOption == 0) {
+					System.out.println("======== No play ========");
+				}
+				else if(checkOption == 1) {
+					System.out.println("======== Ladder comes ========");
+				
+					// adding condition to reach winning position
+					if(playerPosition + diceRoll <= 100) {
+						playerPosition += diceRoll;
+					}
+					else if(playerPosition + diceRoll > 100) {
+					continue;
+					}
+				}
+				else {
+					System.out.println("======== Snake comes ========");
+						if ((playerPosition - diceRoll) < 0) {
+							playerPosition = 0;
+						} else{
+						playerPosition-=diceRoll;
+						}
+		 }			
+	   }	
 	}
 }
